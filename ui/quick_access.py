05d22902c78688ea56f05e9cf0a8434d1d72359f
Yusarina <hello@yusarina.xyz>
2024-06-30 13:07:43 +0100

    FBX Import
    - Add's the ability to import FBX Models.

--- ui/quick_access.py
+++ ui/quick_access.py
@@ -4,6 +4,7 @@ from .panel import AvatarToolkitPanel
 
 from ..core.import_pmx import import_pmx
 from ..core.import_pmd import import_pmd
+from ..core.importer import import_fbx
 
 @register_wrap
 class AvatarToolkitQuickAccessPanel(bpy.types.Panel):
@@ -45,6 +46,8 @@ class AVATAR_TOOLKIT_OT_import_menu(bpy.types.Operator):
         layout.label(text="Select Import Method")
         layout.operator("avatar_toolkit.import_pmx", text="Import PMX")
         layout.operator("avatar_toolkit.import_pmd", text="Import PMD")
+        layout.operator("avatar_toolkit.import_fbx", text="Import FBX") 
+
 
 @register_wrap
 class AVATAR_TOOLKIT_OT_export_menu(bpy.types.Operator):
@@ -86,6 +89,21 @@ class AVATAR_TOOLKIT_OT_import_pmd(bpy.types.Operator):
 
     def execute(self, context):
         import_pmd(self.filepath)
+        return {'FINISHED'}
+
+    def invoke(self, context, event):
+        context.window_manager.fileselect_add(self)
+        return {'RUNNING_MODAL'}
+
+@register_wrap
+class AVATAR_TOOLKIT_OT_import_fbx(bpy.types.Operator):
+    bl_idname = "avatar_toolkit.import_fbx"
+    bl_label = "Import FBX"
+
+    filepath: bpy.props.StringProperty(subtype="FILE_PATH")
+
+    def execute(self, context):
+        import_fbx(self.filepath)
         return {'FINISHED'}
 
     def invoke(self, context, event):
